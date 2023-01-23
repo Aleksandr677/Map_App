81273455517cf2b031d40ab344a603911001c0c5
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2023-01-23 20:22:05 +0300

    Title: add the project
    Body: just add the project

---
+++ Mappy/Mappy/MappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappyApp.swift
+//  Mappy
+//
+//  Created by Христиченко Александр on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct MappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
