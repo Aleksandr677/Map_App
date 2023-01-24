df9da47d2369cc1b5210a76ae5a64e2434a3dfe2
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2023-01-24 13:16:41 +0300

    feat: update LocationsViewModel and LocationView

--- Mappy/Mappy/ViewModels/LocationsViewModel.swift
+++ Mappy/Mappy/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,37 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    //All location
     @Published var locations: [Location]
+    
+    //Current location
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    //
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
     }
 }
